9f8a43f78d2d53de9f5f50a0f55522ec7fbdc0cc
unknown <daniellsilva@outlook.com>
2024-07-13 10:05:49 -0300

    final

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,24 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
-
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+    
+    document.getElementById('execute').addEventListener('click', () => {
+        document.getElementById('titulo').classList.add('pronto');
+    });
+
+    document.getElementById('send').addEventListener('click', sendToServer);
+}
+
+function sendToServer() {
+    let objRequest = new XMLHttpRequest();
+    objRequest.onload = showResponse;
+    objRequest.open("get", "https://jsonplaceholder.typicode.com/posts/1", true);
+    objRequest.send();
+}
+
+function showResponse() {
+    let responseTag = document.getElementById('response');
+    responseTag.innerHTML = (this.responseText);
 }
